2a4f7c7a2dd637974d741508ae8b6cace50f285a
tziont <tzion@worldwideprep.com>
2021-09-25 16:46:43 +0300

    conecting to mongoDb, adding mongoose to the project, insert item to DB

--- src/products/products.module.ts
+++ src/products/products.module.ts
@@ -1,8 +1,11 @@
-import { Module } from "@nestjs/common";
-import { ProductsController } from "./products.controller";
-import { ProductsService } from "./products.service";
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { ProductSchema } from './product.Model';
+import { ProductsController } from './products.controller';
+import { ProductsService } from './products.service';
 
 @Module({
+    imports: [MongooseModule.forFeature([{name: 'Product', schema: ProductSchema}])],
     controllers: [ProductsController],
     providers: [ProductsService]
 })
--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -1,18 +1,28 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
-import { Product } from './product.Model'
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Product } from './product.Model';
 
 @Injectable()
 export class ProductsService {
     private products: Product[] = [];
-    insertProduct(title, description, price) {
+
+    constructor(@InjectModel('Product') private readonly productModel: Model<Product>) { }
+
+ async insertProduct(title, description, price) {
         const {
             v1: uuidv1,
             v4: uuidv4,
         } = require('uuid');
         let prodId = uuidv4();
-        const newProduct = new Product(prodId, title, description, price)
-        this.products.push(newProduct);
-        return prodId;
+        const newProduct = new this.productModel({
+            title,
+            description,
+            price
+        });
+        const result = await newProduct.save();
+        console.log(result);
+        return 'prodId';
     }
 
     getProcucts() {
